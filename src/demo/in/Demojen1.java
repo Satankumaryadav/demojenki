240cd1642cd53b289becc2652f1a486286afd4c4
Satankumaryadav <satankumaryadav27@gmail.com>
2023-08-11 14:42:49 +0530

    commit firstone

---
+++ src/demo/in/Demojen1.java
@@ -0,0 +1,11 @@
+package demo.in;
+
+public class Demojen1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this first build of java at jenkin");
+
+	}
+
+}
