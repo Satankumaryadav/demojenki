738f9027c9ef4da4132135d63dde64354d67d332
Satankumaryadav <satankumaryadav27@gmail.com>
2023-08-11 15:03:37 +0530

    made chnage2

--- src/demo/in/Demojen1.java
+++ src/demo/in/Demojen1.java
@@ -5,6 +5,7 @@ public class Demojen1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this first build of java at jenkin");
+		System.out.println("this first build of java at jenkin");
 
 	}
 
